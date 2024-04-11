f7dc7a2bc24eab37a25c7d2ec1d096423960e23a
mahya rastegari <mahyarastegarii@gmail.com>
2024-04-11 16:45:38 +0330

    create Project & install the necessary libraries

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="container">
+        <h2> Admin Dashboard </h2>
+      </div>
+    </>
+  );
+}
+
+export default App;
