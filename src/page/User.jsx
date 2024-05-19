97feb08a0a35992e4da0e355bb971520d665681d
mahya rastegari <mahyarastegarii@gmail.com>
2024-05-20 00:26:48 +0330

    User Page Design

---
+++ src/page/User.jsx
@@ -0,0 +1,329 @@
+import { FilterList, Search, Sort } from "@mui/icons-material";
+import {
+  Avatar,
+  Paper,
+  styled,
+  Table,
+  TableBody,
+  TableCell,
+  tableCellClasses,
+  TableContainer,
+  TableFooter,
+  TableHead,
+  TablePagination,
+  TableRow,
+  Stack,
+  Typography,
+  TableSortLabel,
+  Box,
+  InputBase,
+  IconButton
+} from "@mui/material";
+import  React, { useState  } from "react";
+import SearchBox from "../components/search/SearchBox";
+
+export default function User() {
+
+  
+
+   const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(5);
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(+event.target.value);
+    setPage(0);
+  };
+
+  const StyledTableCell = styled(TableCell)(({ theme }) => ({
+    [`&.${tableCellClasses.head}`]: {
+      backgroundColor: theme.palette.grey[400],
+      color: theme.palette.common.black,
+      
+    },
+    [`&.${tableCellClasses.body}`]: {
+      fontSize: 14,
+    
+    },
+  }));
+  const headCells = [
+  
+    {
+      id: 1,
+      label: "name",
+      align: "left",
+      minWidth: 130,
+    },
+    // {
+    //   id: 2,
+    //   label: "lastName",
+    //   align: "center",
+    //   minWidth: 130,
+    // },
+    {
+      id: 3,
+      label: "email",
+      align: "center",
+      minWidth: 210,
+    },
+    {
+      id: 4,
+      label: "phone Number",
+      align: "center",
+      minWidth: 120,
+    },
+    {
+      id: 5,
+      label: "number Of Courses",
+      align: "center",
+      minWidth: 10,
+    },
+  ];
+
+  const rows = [
+    {
+      id: 1,
+      avatar: "M",
+      name: "mahya rastegari",
+      // lastName: "rastegari",
+      email: "mahyarastegarii@gmail.com",
+      phone: "093022222033",
+      numberOfCourses: 4,
+    },
+    {
+      id: 2,
+      avatar: "M",
+      name: "mahya rastegari",
+      // lastName: "rastegari",
+      email: "mahyarastegarii@gmail.com",
+      phone: "093022222033",
+      numberOfCourses: 4,
+    },
+    {
+      id: 3,
+      avatar: "M",
+      name: "mahya rastegari",
+      // lastName: "rastegari",
+      email: "mahyarastegarii@gmail.com",
+      phone: "093022222033",
+      numberOfCourses: 4,
+    },
+    {
+      id: 4,
+      avatar: "M",
+      name: "mahya rastegari",
+      // lastName: "rastegari",
+      email: "mahyarastegarii@gmail.com",
+      phone: "093022222033",
+      numberOfCourses: 4,
+    },
+    {
+      id: 5,
+      avatar: "M",
+      name: "mahya rastegari",
+      // lastName: "rastegari",
+      email: "mahyarastegarii@gmail.com",
+      phone: "093022222033",
+      numberOfCourses: 5,
+    },
+    {
+      id: 6,
+      avatar: "M", 
+      name: "mahya rastegari",
+      // lastName: "rastegari",
+      email: "mahyarastegarii@gmail.com",
+      phone: "093022222033",
+      numberOfCourses: 8,
+    },
+    {
+      id: 7,
+      avatar: "M",
+      name: "mahya rastegari",
+      // lastName: "rastegari",
+      email: "mahyarastegarii@gmail.com",
+      phone: "093022222033",
+      numberOfCourses: 4,
+    },
+    {
+      id: 8,
+      avatar: "M",
+      name: "mahya rastegari",
+      // lastName: "rastegari",
+      email: "mahyarastegarii@gmail.com",
+      phone: "093022222033",
+      numberOfCourses: 4,
+    },
+    {
+      id: 9,
+      avatar: "M",
+      name: "mahya rastegari",
+      // lastName: "rastegari",
+      email: "mahyarastegarii@gmail.com",
+      phone: "093022222033",
+      numberOfCourses: 4,
+    },
+    {
+      id: 10,
+      avatar: "M",
+      name: "mahya rastegari",
+      // lastName: "rastegari",
+      email: "mahyarastegarii@gmail.com",
+      phone: "093022222033",
+      numberOfCourses: 4,
+    },
+    {
+      id: 11,
+      avatar: "M",
+      name: "mahya rastegari",
+      // lastName: "rastegari",
+      email: "mahyarastegarii@gmail.com",
+      phone: "093022222033",
+      numberOfCourses: 4,
+    },
+    {
+      id: 12,
+      avatar: "M",
+      name: "mahya rastegari",
+      // lastName: "rastegari",
+      email: "mahyarastegarii@gmail.com",
+      phone: "093022222033",
+      numberOfCourses: 4,
+    },
+    {
+      id: 13,
+      avatar: "M",
+      name: "mahya rastegari",
+      // lastName: "rastegari",
+      email: "mahyarastegarii@gmail.com",
+      phone: "093022222033",
+      numberOfCourses: 4,
+    },
+    {
+      id: 14,
+      avatar: "M",
+      name: "mahya rastegari",
+      // lastName: "rastegari",
+      email: "mahyarastegarii@gmail.com",
+      phone: "093022222033",
+      numberOfCourses: 4,
+    },
+    {
+      id: 15,
+      avatar: "M",
+      name: "mahya rastegari",
+      // lastName: "rastegari",
+      email: "mahyarastegarii@gmail.com",
+      phone: "093022222033",
+      numberOfCourses: 4,
+    },
+  ];
+
+ 
+
+
+ 
+
+  
+  const rowTable = rows.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+
+  return (
+    <Box component={Paper} elevation={2}>
+      <Stack direction="row" spacing={2} sx={{ padding: 2}} >
+      <IconButton aria-label="Sort">
+      <Sort/>
+  
+      </IconButton>
+       
+    <SearchBox/>
+    
+      
+      </Stack>
+    <TableContainer  sx={{width: "100%"}}>
+      <Table >
+        <TableHead>
+       
+
+          <TableRow  >
+            {headCells.map((headCell) => (
+              <StyledTableCell
+                key={headCell.id}
+                align={headCell.align}
+                style={{ minWidth: headCell.minWidth }}
+              >
+
+              {/* {
+                headCell.label === "name" ?  
+                <TableSortLabel   > 
+             <Typography variant="body1" component="h2">
+
+                {headCell.label}
+                </Typography>
+                </TableSortLabel> :
+                
+                <Typography variant="body1" component="h2">
+                {headCell.label}
+                </Typography>
+              } */}
+            <Typography variant="body1" component="h2">
+                {headCell.label}
+                </Typography>
+                
+              </StyledTableCell>
+            ))}
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rowTable.map((item) => (
+            <TableRow hover sx={{}} key={item.id} >
+               {/* <TableCell align="center"><Avatar>{ item.avatar}</Avatar></TableCell> */}
+              <TableCell align="center">
+              <Stack direction="row" spacing={3} display="flex" alignItems="center ">
+              <Avatar>{ item.avatar}</Avatar>
+                <Typography variant="body2">
+                 {item.name}
+                </Typography> 
+                </Stack>
+              </TableCell>
+              {/* <TableCell align="center">{item.lastName}</TableCell> */}
+              <TableCell align="center">
+                 <Typography variant="body2">
+                 {item.email}
+                </Typography> 
+                </TableCell>
+              <TableCell align="center">
+              <Typography variant="body2">
+              {item.phone}
+                </Typography> 
+              </TableCell>
+              <TableCell align="center">
+               
+
+                <Typography variant="body2">
+                {item.numberOfCourses}  course
+                </Typography> 
+                </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+        <TableFooter >
+          <TableRow >
+          <TablePagination
+        rowsPerPageOptions={[5, 10, 15]}
+        // colSpan={3}
+      
+        count={rows.length}
+        rowsPerPage={rowsPerPage}
+        page={page}
+        onPageChange={handleChangePage}
+        onRowsPerPageChange={handleChangeRowsPerPage}
+      />
+          </TableRow>
+        </TableFooter>
+      </Table>
+    </TableContainer>
+    </Box>
+  );
+}
