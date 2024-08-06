af131e854379e7f7f4127b3171bc00c51acbaa46
mahya rastegari <mahyarastegarii@gmail.com>
2024-08-06 23:49:48 +0330

    Connect To  the Database

---
+++ src/core/createClient.js
@@ -0,0 +1,7 @@
+import { createClient } from '@supabase/supabase-js';
+
+const supabaseURL = "https://qjfoypokbphqxocozpfj.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFqZm95cG9rYnBocXhvY296cGZqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjIxNzAzMjcsImV4cCI6MjAzNzc0NjMyN30.qgjLGB3AaZpE7hr3rHQKdqvPq--WHlGXTVW3D0WOVZc";
+
+  export const supabase = createClient(supabaseURL, supabaseKey);
