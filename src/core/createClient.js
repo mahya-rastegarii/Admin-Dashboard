aa9509a877c11f91d09d8560c55947f29fbbe979
mahya <89992059+mahya-rastegarii@users.noreply.github.com>
2026-05-31 16:05:55 +0330

    Update createClient.js

--- src/core/createClient.js
+++ src/core/createClient.js
@@ -1,8 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
 
-const supabaseURL = "https://qjfoypokbphqxocozpfj.supabase.co";
-const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFqZm95cG9rYnBocXhvY296cGZqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjIxNzAzMjcsImV4cCI6MjAzNzc0NjMyN30.qgjLGB3AaZpE7hr3rHQKdqvPq--WHlGXTVW3D0WOVZc";
+const supabaseURL = import.meta.env.VITE_PUBLIC_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_PUBLIC_SUPABASE_ANON_KEY;
 
   export const supabase = createClient(supabaseURL, supabaseKey);
